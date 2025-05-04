501a6b7a636313ee2ce1e9155c4adb1c9863c0b4
unknown <rprak9047@gmail.com>
2025-05-04 21:47:35 +0530

    Initial commit

---
+++ django-bugtracker-master/bugtracker/models.py
@@ -0,0 +1,45 @@
+"""Models used to store tickets."""
+from django.conf import settings
+from django.db import models
+
+STATUSES = (('I', 'Initial'),
+            ('A', 'Awaiting Update'),
+            ('F', 'Fixed'),
+            ('W', 'Won\'t Fix'))
+
+PRIORITIES = (('L', 'Low'),
+              ('M', 'Medium'),
+              ('H', 'High'))
+
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    attachment = models.FileField(upload_to='bugtracker', blank=True, null=True)
+    status = models.CharField(max_length=1, choices=STATUSES)
+    priority = models.CharField(max_length=1, choices=PRIORITIES)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='+', on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='+', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-updated_time']
+
+    def __unicode__(self):
+        return self.title
+
+
+class TicketUpdate(models.Model):
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    update_text = models.TextField()
+    attachment = models.FileField(upload_to='bugtracker', blank=True, null=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='+', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-updated_time']
+
+    def __unicode__(self):
+        return '%s-update-%s' % (self.ticket.title, self.ticket.id)
---
+++ django-bugtracker-master/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+import bugtracker
+
+version = bugtracker.__version__
+long_description = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+setup(
+    name = 'django-bugtracker',
+    version = version,
+    packages = ['bugtracker'],
+    include_package_data = True,
+    license = 'BSD License',
+    description = 'A simple Django admin-based bug tracker app.',
+    long_description = long_description,
+    url = 'https://github.com/ahernp/django-bugtracker',
+    author = 'Paul Ahern',
+    author_email = 'ahernp@ahernp.com',
+    classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        'Django>=1.6.2',
+    ],
+)
